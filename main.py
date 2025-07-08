da0503a0e551629fa45f512a1535069dc784d911
ishaan-ansari <ishaanansari044@gmail.com>
2025-07-08 18:10:03 +0530

    Implemented content_writer agent

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ llm = LLM(model='gpt-4')
 # tool 2
 search_tool = SerperDevTool(n=10)
 
+# Agent 1
 research_analyst = Agent(
-    role="Blog researcher from Youtube videos",
+    role="Seasoned and the best Research analyst of the world",
     goal=f'Research, analyze, and synthesize comprehensive information on {topic} from reliable web sources',
     verbose=True,
     memory=True,
@@ -33,6 +34,23 @@ research_analyst = Agent(
     allow_deligations=False,
     llm=llm
 )
+
+# Agent 2
+content_writer = Agent(
+    role="Senior content writer",
+    goal="Transform, research findings into engaging blog post while maintaining accuracy",
+    backstory="You're are skilled content writer specialized in creating"
+        "engaging, accessible content from technical research. "
+        "You work closely with the Senior Research Analyst and excel at maintaining the perfect "
+        "balance between informative and entertaining writing, "
+        "while ensuring all facts and citations from the research "
+        "are properly incorporated. You have a talent for making "
+        "complex topics approachable without oversimplifying them.",
+    allow_delegation=False,
+    verbose=True,
+    llm=llm
+)
+
 
 
 """
